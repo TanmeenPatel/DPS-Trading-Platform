b49d92279894d63585ea517d8b26e09cc6ec716a
Faizaan-Nasir <82143161+Faizaan-Nasir@users.noreply.github.com>
2022-11-13 13:00:30 +0400

    removed alerts, new alerts look somewhat better @TanmeenPatel may perfect em

--- scripts/news.js
+++ scripts/news.js
@@ -1,8 +1,9 @@
 let NewsTrip = [
-    { News: "Reliance digital decides to offer 5G services to all of it's users.", P: "5G succeeds in India.", N: "5G fails in India." },
+    { News: "Reliance digital decides to offer 5G services to all of it's users.", P: "5G succeeds in India. ", N: "5G fails in India." },
     { News: "AMD radeon releases their first ever graphics drive in India. It looks cool!", P: "AMD Radeon does not do well for graphic drivers in India.", N: "AMD Radeon succeeds in selling graphic drivers in India." },
     { News: "Tesla gets permission to manufacture cars in India.", P: "People's opinion has shifted to support electric cars in India.", N: "Indians boycott electric cars." },
-    { News: "A desperate attempt is made to revive Air India.", P: "An Air India aircraft landed safely into Mumbai after a dual engine failure.", N: "Pilots boycott Vistara due to crew harassment complaints." }
+    { News: "Tata decides to buy two major Indian airlines: Air India and Vistara", P: "An Air India aircraft landed safely into Mumbai after a dual engine failure.", N: "Pilots boycott Vistara due to crew harassment complaints." },
+    { News: "Microsoft releases windows 12 in India.", P: "Windows 12 update is so good looking. Everyone Adopts windows 12.", N: "No one feels the need to update to windows 12. " },
 ]
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -13,13 +14,23 @@ function getRandomArbitrary(min, max) {
 i = 0
 function loop() {
     setTimeout(function () {
+        setTimeout(function () {
+            document.getElementById("news").innerHTML = ""
+            document.getElementById("news-info").style.display = "none"
+        }, 5000)
         let p = `p${i + 1}`
         if (i < NewsTrip.length) {
-            alert(NewsTrip[i].News)
+            document.getElementById("news").innerHTML = NewsTrip[i].News
+            document.getElementById("news-info").style.display = "block"
             setTimeout(function () {
                 let random = getRandomInt(2)
                 if (random == 0) {
-                    alert(NewsTrip[i].N)
+                    document.getElementById("news").innerHTML = NewsTrip[i].N
+                    document.getElementById("news-info").style.display = "flex"
+                    setTimeout(function () {
+                        document.getElementById("news").innerHTML = ""
+                        document.getElementById("news-info").style.display = "none"
+                    }, 5000)
                     let value = getRandomArbitrary(15, 25)
                     let dec = Number(localStorage[p]) * value / 100
                     localStorage[p] = Number(localStorage[p]) - dec
@@ -27,7 +38,12 @@ function loop() {
                     i++
                 }
                 else {
-                    alert(NewsTrip[i].P)
+                    document.getElementById("news").innerHTML = NewsTrip[i].P
+                    document.getElementById("news-info").style.display = "flex"
+                    setTimeout(function () {
+                        document.getElementById("news").innerHTML = ""
+                        document.getElementById("news-info").style.display = "none"
+                    }, 5000)
                     let value = getRandomArbitrary(15, 40)
                     let inc = Number(localStorage[p]) * value / 100
                     localStorage[p] = Number(localStorage[p]) + inc
