16aed54403ad07ae77e6a6810743b8c4ad89d385
amazinglySK <shashwat07ktm@gmail.com>
2022-11-13 17:42:47 +0400

    Graph fixes

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,4 +1,6 @@
-const labels = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+const labels = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
 
 let buy_config = {
     type: "line",
@@ -13,7 +15,11 @@ let buy_config = {
             },
         ],
     },
-    options: {},
+    options: {
+        legend: {
+            onClick: null,
+        },
+    },
 };
 
 let sell_config = {
@@ -29,7 +35,11 @@ let sell_config = {
             },
         ],
     },
-    options: {},
+    options: {
+        legend: {
+            onClick: null,
+        },
+    },
 };
 
 const buy_chart = new Chart(
@@ -41,28 +51,26 @@ const sell_chart = new Chart(
     sell_config
 );
 
-
-
-
 function register_change(product_name, price) {
     // console.log(product_name)
-    let stock_hist = JSON.parse(localStorage.stock_history)
-    stock_hist[product_name].push(price)
-    localStorage.stock_history = JSON.stringify(stock_hist)
-    console.log(price, product_name)
+    let stock_hist = JSON.parse(localStorage.stock_history);
+    if (stock_hist[product_name].length == 20) {
+        stock_hist[product_name].splice(0, 11);
+    }
+    stock_hist[product_name].push(price);
+    localStorage.stock_history = JSON.stringify(stock_hist);
 }
 
-
-function display_buy_chart(product_name, srl_no){
-    buy_config.data.datasets[0].label = product_name
-    let stock_hist = JSON.parse(localStorage.stock_history)
-    buy_config.data.datasets[0].data = stock_hist[srl_no]
-    buy_chart.update()
+function display_buy_chart(product_name, srl_no) {
+    buy_config.data.datasets[0].label = product_name;
+    let stock_hist = JSON.parse(localStorage.stock_history);
+    buy_config.data.datasets[0].data = stock_hist[srl_no];
+    buy_chart.update();
 }
 
-function display_sell_chart(product_name, srl_no){
-    sell_config.data.datasets[0].label = product_name
-    let stock_hist = JSON.parse(localStorage.stock_history)
-    sell_config.data.datasets[0].data = stock_hist[srl_no]
-    sell_chart.update()
+function display_sell_chart(product_name, srl_no) {
+    sell_config.data.datasets[0].label = product_name;
+    let stock_hist = JSON.parse(localStorage.stock_history);
+    sell_config.data.datasets[0].data = stock_hist[srl_no];
+    sell_chart.update();
 }
